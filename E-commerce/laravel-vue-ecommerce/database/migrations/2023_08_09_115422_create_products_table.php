44be7dface945773ddc0cd680457b5edef791835
Chebet Maryline <77463649+maryline321@users.noreply.github.com>
2023-08-09 22:20:39 +0300

    generated tables

--- E-commerce/laravel-vue-ecommerce/database/migrations/2023_08_09_115422_create_products_table.php
+++ E-commerce/laravel-vue-ecommerce/database/migrations/2023_08_09_115422_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,17 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->String('title',2000);
+            $table->String('slug',2000);
+            $table->String('image',2000)->nullable();
+            $table->String('image_mime')->nullable();
+            $table->String('image_size')->nullable();
+            $table->String('image_description')->nullable();
+            $table->String('price', 10, 2);
+            $table->foreignIdFor(User::class, 'created_by')->nullable();
+            $table->foreignIdFor(User::class, 'updated_by')->nullable();
+            $table->softDelete();
+            $table->foreignIdFor(User::class, 'deleted_by')->nullable();
             $table->timestamps();
         });
     }
