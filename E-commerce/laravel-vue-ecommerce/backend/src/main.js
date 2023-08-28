367ef478a0513c08aa288d020c917b5e843c4944
Chebet Maryline <77463649+maryline321@users.noreply.github.com>
2023-08-28 23:35:39 +0300

    file updates

--- E-commerce/laravel-vue-ecommerce/backend/src/main.js
+++ E-commerce/laravel-vue-ecommerce/backend/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-createApp(App).mount('#app')
+createApp(App)
+
+.use(store)
+.mount('#app')
