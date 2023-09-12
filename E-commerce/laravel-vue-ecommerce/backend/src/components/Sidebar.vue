ac0c5cfe408d25ba118bbce8301ff952049fb16d
Chebet Maryline <77463649+maryline321@users.noreply.github.com>
2023-09-12 23:35:38 +0300

    Update on sidebar

--- E-commerce/laravel-vue-ecommerce/backend/src/components/Sidebar.vue
+++ E-commerce/laravel-vue-ecommerce/backend/src/components/Sidebar.vue
@@ -1,18 +1,4 @@
 <template>
-<!-- <html>
-
-<head>
-    <meta charset="UTF-8" />
-     <link href="src/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-     <link
-         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-         rel="stylesheet">
- 
-    
-     <link href="src/assets/css/sb-admin-2.min.css" rel="stylesheet">
-     <link href="src/assets/css/sb-admin-2.min.css" rel="stylesheet">
-  </head>
-</html> -->
 
     <body id="page-top">
     
